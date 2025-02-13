d48e1025485dae3a6a447f71f82cfd575bcf0007
akash-kumar-s0 <akashkumars@presidio.com>
2025-02-13 12:47:43 +0530

    tested how lock works

--- day-3/task1/main.tf
+++ day-3/task1/main.tf
@@ -1,3 +1,8 @@
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "ak-terraform-bucket" 
+# resource "aws_s3_bucket" "my_bucket" {
+#   bucket = "ak-terraform-bucket" 
+# }
+
+resource "time_sleep" "name" {
+  create_duration = "100s"
+  destroy_duration = "100s"
 }
